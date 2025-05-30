99bd03351d65634b7eaa3bad6813d35f911440a1
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-05-30 12:58:57 -0500

    Add new group to users

--- users.sh
+++ users.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+LOG_GROUP="auditlog"
+
 for user in muskan anjali siddharth vanya svetlana vitaly vladimir;do 
   useradd -m -s /bin/bash "${user}" 
   echo "${user}:1234" | chpasswd
   adduser "${user}" sudo
+  adduser "${user}" "$LOG_GROUP"
 done
